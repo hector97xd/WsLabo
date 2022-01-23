2847e611ec0a381d955d8e89206d8c991f193694
Hector <hector.guevarah97@gmail.com>
2022-01-23 11:37:39 -0600

    cors

--- WsLabo/Program.cs
+++ WsLabo/Program.cs
@@ -1,15 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using WsLabo.Context;
-
+using Microsoft.AspNetCore.Cors;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 //builder.WebHost.ConfigureKestrel(options => options.ListenLocalhost(5001));
 var service = builder.Services.AddControllers();
+string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors();
+builder.Services.AddCors(option =>
+    option.AddPolicy(name: MyAllowSpecificOrigins,
+                              builder =>
+                              {
+                                  builder.WithOrigins("http://localhost",
+                                                      "http://127.0.0.1");
+                              }));
+builder.Services.AddControllers();
 var connectionString = builder.Configuration.GetConnectionString("AppDb");
 builder.Services.AddDbContext<LaboDbContext>(x=> x.UseSqlServer(connectionString));
 var app = builder.Build();
@@ -21,12 +29,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseRouting();
 app.UseHttpsRedirection();
-app.UseCors(x => x
-    .AllowAnyHeader()
-    .AllowAnyMethod()
-    .AllowAnyOrigin());
-app.MapControllers();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCors(MyAllowSpecificOrigins);
 
 app.Run();
